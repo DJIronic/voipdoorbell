64e68e444ec64a00e715a5e9226cd426a59b3814
Šimon Čecháček <dj@life-games.cz>
2021-12-08 20:18:03 +0100

    Added PJSIP & PJSUA2 files used
    PJSIP files with my edits to work with my code. Put in in separate folder for licensing issues.

---
+++ pjsip-files/endpoint.py
@@ -0,0 +1,39 @@
+# $Id$
+#
+# pjsua Python GUI Demo
+#
+# Copyright (C)2013 Teluu Inc. (http://www.teluu.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+#
+import sys
+
+import pjsua2 as pj
+
+class Endpoint(pj.Endpoint):
+    """
+    This is high level Python object inherited from pj.Endpoint
+    """
+    instance = None
+    def __init__(self):
+        pj.Endpoint.__init__(self)
+        Endpoint.instance = self
+
+
+def validateUri(uri):
+    return Endpoint.instance.utilVerifyUri(uri) == pj.PJ_SUCCESS
+
+def validateSipUri(uri):
+    return Endpoint.instance.utilVerifySipUri(uri) == pj.PJ_SUCCESS
